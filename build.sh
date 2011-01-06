df62fd5f21fed913de613172c97ecf65694ab080
Scott McLeod <halcyonblue@gmail.com>
2011-01-06 00:23:41 +0000

    Updating build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y build-essential bison flex
-cd heirloom-sh ; make install ; cd ..
-cd heirloom ; make install ; cd ..
-cd heirloom-pkgtools ; make install ; cd ..
+aptitude install -y build-essential bison flex ed
+cd heirloom-sh ; make ; make install ; cd ..
+cd heirloom ; make ; make install ; cd ..
+cd heirloom-pkgtools ; make; make install ; cd ..
